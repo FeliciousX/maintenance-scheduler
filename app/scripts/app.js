c0c43356cbf42a6dbcdf6ab3d84522f4e504996d
FeliciousX <feliciousx@gmail.com>
2013-11-26 15:42:42 +0800

    Adds a new route:login Sample of angular-ui-calendar is displayed at login route TODO: Change the calendar to display appropriately

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,13 +4,18 @@ angular.module('maintenenceSchedulerApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ui.calendar'
 ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
       })
       .otherwise({
         redirectTo: '/'
