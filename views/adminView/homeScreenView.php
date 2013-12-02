525efc407b2898e5cc16376f113bc41619140e69
FeliciousX <feliciousx@gmail.com>
2013-12-02 23:19:00 +0800

    The calendar will be displayed with static data o Calendar with some data from real calendars online

--- views/adminView/homeScreenView.php
+++ views/adminView/homeScreenView.php
@@ -1,8 +1,18 @@
-
-<p>Schedule</p></br>
-<button class="btn btn-primary"> Generate Schedule</button></br></br>
-<div class = "well"> 
-  <h2>FILTERS</h2>
+<script type="text/javascript" src="../includes/js/angular/angular.min.js"></script>
+<script type="text/javascript" src="../includes/js/jquery-ui/jquery-ui.js"></script>
+<script type="text/javascript" src="../includes/js/angular/ui-calendar/calendar.js"></script>
+<script type="text/javascript" src="../includes/js/angular/fullcalendar/fullcalendar.min.js"></script>
+<script type="text/javascript" src="../includes/js/angular/fullcalendar/gcal.js"></script>
+<script type="text/javascript" src="../includes/js/calendar.js"></script>
+<link rel="stylesheet" href="../includes/css/fullcalendar.css" />
+<div ng-app="maintenenceSchedulerApp">
+	<div ng-controller="CalendarCtrl">
+		<p>Schedule</p></br>
+		<button class="btn btn-primary"> Generate Schedule</button></br></br>
+		<div class = "well">
+		  <h2>FILTERS</h2>
+		</div>
+		</br></br>
+		<div ui-calendar="uiConfig.calendar" class="calendar" ng-model="eventSources"></div>
+	</div>
 </div>
-</br></br>
-<h1>Calendar Magic!</h1>
