d57569e9d718b27673fb23678ea80398fb949d4d
DieHard073055 <eshanshafeeq073055@gmail.com>
2013-12-09 12:23:23 +0800

    Still need to fix the admin/homescreen the angular js part the post requeest wont get the data

--- controllers/scheduleController.php
+++ controllers/scheduleController.php
@@ -1,134 +1,155 @@
 <?php 
 
-  /**
-  * 
-  */
-  class scheduleController
-  {
-    
-    function addScheduleTask($taskname, $taskDesc, $taskDue, $invMps){
-      if(!isset($_SESSION)){
-          session_start();
-      }
-      $task_assigned= date('Y-m-d H:i:s');
+	/**
+	* 
+	*/
+	class scheduleController
+	{
+		
+		function addScheduleTask($taskname, $taskDesc, $taskDue, $invMps){
+			if(!isset($_SESSION)){
+					session_start();
+			}
+			$task_assigned= date('Y-m-d H:i:s');
 
 
-      $_SESSION['schedule']['last_id'] = intval($_SESSION['schedule']['last_id']) + 1;
+			$_SESSION['schedule']['last_id'] = intval($_SESSION['schedule']['last_id']) + 1;
 
-      $data = array(
-        'task_id' => $_SESSION['schedule']['last_id'],
-        'task_name' => $taskname,
-        'task_description'  => $taskDesc,
-        'task_assigned' => $task_assigned,
-        'task_due'  => $taskDue,
-        'involved_mp' => $invMps,
-        'new_task' => 'true'
-       );
-      array_push( $_SESSION['schedule_task'], $data);
-      
-    }
-    
+			$data = array(
+				'task_id' => $_SESSION['schedule']['last_id'],
+				'task_name' => $taskname,
+				'task_description'	=> $taskDesc,
+				'task_assigned' => $task_assigned,
+				'task_due'	=> $taskDue,
+				'involved_mp' => $invMps,
+				'new_task' => 'true'
+			 );
+			array_push( $_SESSION['schedule_task'], $data);
+			
+		}
+		
 
-    function getSchduleList(){
+		function getSchduleList(){
 
-      if(!isset($_SESSION)){
-          session_start();
-        }
-      if(isset($_SESSION['schedule_task'])){
-        return $_SESSION['schedule_task'];
-      }else{
-        $db_controller = new DatabaseController();
-        return $db_controller->getScheduleTaskList();
-      }
-    }
+			if(!isset($_SESSION)){
+					session_start();
+				}
+			if(isset($_SESSION['schedule_task'])){
+				return $_SESSION['schedule_task'];
+			}else{
+				$db_controller = new DatabaseController();
+				return $db_controller->getScheduleTaskList();
+			}
+		}
 
-    function editSchedule(){
-      $task_id = $_POST['task_id'];
+		function editSchedule(){
+			$task_id = $_POST['task_id'];
 
-      if(!isset($_SESSION)){
-          session_start();
-        }
-      if(isset($_SESSION['schedule_task'])){
-        $data = $_SESSION['schedule_task'];
+			if(!isset($_SESSION)){
+					session_start();
+				}
+			if(isset($_SESSION['schedule_task'])){
+				$data = $_SESSION['schedule_task'];
 
-        for ($i=0; $i < sizeof($data); $i++) { 
-          if($data[$i]['task_id'] == $task_id){
-            $_SESSION['schedule_task'][$i]['task_id'] =$task_id ;
-            $_SESSION['schedule_task'][$i]['task_name'] = $_POST['task_name'];
-            $_SESSION['schedule_task'][$i]['task_description']  = $_POST['task_description'];
-            $_SESSION['schedule_task'][$i]['task_assigned'] = $_POST['task_assigned'];
-            $_SESSION['schedule_task'][$i]['task_due']  = $_POST['task_due'];
-            $_SESSION['schedule_task'][$i]['involved_mp'] = $_POST['involved_mp'];
+				for ($i=0; $i < sizeof($data); $i++) { 
+					if($data[$i]['task_id'] == $task_id){
+						$_SESSION['schedule_task'][$i]['task_id'] =$task_id ;
+						$_SESSION['schedule_task'][$i]['task_name'] = $_POST['task_name'];
+						$_SESSION['schedule_task'][$i]['task_description']	= $_POST['task_description'];
+						$_SESSION['schedule_task'][$i]['task_assigned'] = $_POST['task_assigned'];
+						$_SESSION['schedule_task'][$i]['task_due']	= $_POST['task_due'];
+						$_SESSION['schedule_task'][$i]['involved_mp'] = $_POST['involved_mp'];
 
-            if (isset($_POST['task_done'])) {
-              $_SESSION['schedule_task'][$i]['task_done'] = $_POST['task_done'];
-            }
-            if (isset($_POST['feedback_msg_id'])) {
-              $_SESSION['schedule_task'][$i]['feedback_msg_id'] = $_POST['feedback_msg_id'];
-            }
+						if (isset($_POST['task_done'])) {
+							$_SESSION['schedule_task'][$i]['task_done'] = $_POST['task_done'];
+						}
+						if (isset($_POST['feedback_msg_id'])) {
+							$_SESSION['schedule_task'][$i]['feedback_msg_id'] = $_POST['feedback_msg_id'];
+						}
 
-          }          
-        }
+					}					
+				}
 
-        header("Location: ../views/adminView.php?editScheduleTask=1");
-      }else{
+				header("Location: ../views/adminView.php?editScheduleTask=1");
+			}else{
 
-        header("Location: ../views/adminView.php?editScheduleTask=0");
-      }
+			header("Location: ../views/adminView.php?editScheduleTask=0");
+		}
 
-    }
-  }
-
+		}
+		function adminCalendar(){
+			$scheduleList = $this->getSchduleList();
+			$echolist = array();
+			$count = 0;
+			echo '<meta charset="UTF-8">';
+			foreach ($scheduleList as $task ){
+				
+				$taskName = $task['task_name'];
+				$date = $task['task_due'];
+				$list = array('title' => $taskName, 'date'=>$date);
+				array_push($echolist, $list);
+			}
+				echo '<div>';
+				echo json_encode($echolist);
+				echo '</div>';
+		}
+	}
+		
 
 
 /*=====================================
 
-              Main Function
+							Main Function
 
 =======================================*/
 
 if(!empty($_POST)){
-    $type = $_POST['type'];
-    $scheduleCtrl = new scheduleController();
+		$type = $_POST['type'];
+		$scheduleCtrl = new scheduleController();
 
-    switch ($type) {
-      case 'add':
-        $name = $_POST['name'];
-        $date = $_POST['date'];
-        $personnel = $_POST['personnel'];
-        $desc = $_POST['desc'];
+		switch ($type) {
+			case 'add':
+				$name = $_POST['name'];
+				$date = $_POST['date'];
+				$personnel = $_POST['personnel'];
+				$desc = $_POST['desc'];
 
 
-        $scheduleCtrl->addScheduleTask($name, $desc, $date, $personnel );
-        header("Location: ../views/adminView.php?addScheduleTask=1");
-        break;
+				$scheduleCtrl->addScheduleTask($name, $desc, $date, $personnel );
+				header("Location: ../views/adminView.php?addScheduleTask=1");
+				break;
 
-      case 'edit':
-        echo json_encode($_POST);
-        $scheduleCtrl->editSchedule();
-        break;
+			case 'edit':
+				echo json_encode($_POST);
+				$scheduleCtrl->editSchedule();
+				break;
 
-      case 'remove':
-        break;
-
-    }
+		case 'remove':
+			break;
+			
+		case 'calendarAdmin':
+			$scheduleController->adminCalendar();
+			break;
+		default:
+			echo json_encode('lol');
+		}
 }
 if(!empty($_GET)){
-  if(!isset($_SESSION)){
-    session_start();
-  }
-  if(isset($_GET['removeSchedule'])){
-    $task_id = $_GET['removeSchedule'];
+	if(!isset($_SESSION)){
+		session_start();
+	}
+	if(isset($_GET['removeSchedule'])){
+		$task_id = $_GET['removeSchedule'];
 
-    for ($i=0; $i < sizeof($_SESSION['schedule_task']); $i++) { 
-      if($_SESSION['schedule_task'][$i]['task_id'] == $task_id){
-        unset($_SESSION['schedule_task'][$i]);
-        break;
-      }
-    }
+		for ($i=0; $i < sizeof($_SESSION['schedule_task']); $i++) { 
+			if($_SESSION['schedule_task'][$i]['task_id'] == $task_id){
+				unset($_SESSION['schedule_task'][$i]);
+				break;
+			}
+		}
 
-    header("Location: ../views/adminView.php?removeScheduleTask=1");
-  }
+		header("Location: ../views/adminView.php?removeScheduleTask=1");
+	}
 }
 
 
--- includes/js/calendar.js
+++ includes/js/calendar.js
@@ -14,13 +14,14 @@ function CalendarCtrl($scope) {
             currentTimezone: 'America/Chicago' // an option!
     };
     /* event source that contains custom events on the scope */
-    $scope.events = [
+    $scope.events =[
       {title: 'All Day Event',start: new Date(y, m, 1)},
       {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
       {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
       {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
       {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-      {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+      {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'},
+      {title: 'Diehard Event',start: new Date(2013, 11, 20),allday:false}
     ];
     /* event source that calls a function on every view switch */
     $scope.eventsF = function (start, end, callback) {
